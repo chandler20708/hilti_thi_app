f278f3a2eea48523c47934c6a35543a8d13365fc
chandlerliuliu <chandlerliu@tmrmds.co>
2026-07-02 23:27:49 +0100

    Add author and GitHub footer

--- Overview.py
+++ Overview.py
@@ -12,3 +12,13 @@ st.set_page_config(
 
 st.markdown(APP_CSS, unsafe_allow_html=True)
 render_market_page()
+st.divider()
+st.markdown(
+    """
+    <div style="text-align: center; font-size: 0.95rem; color: #5b6472; padding: 0.25rem 0 1rem 0;">
+      © 2025-2026 <b>Chia-Te Liu</b>. Author of this app.<br>
+      Source code is maintained on GitHub and this app is hosted via Streamlit Community Cloud.
+    </div>
+    """,
+    unsafe_allow_html=True,
+)
